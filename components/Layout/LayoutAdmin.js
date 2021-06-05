865143fb13324d84edf7390186af59c44174d857
andresdev <andresmestra11a@gmail.com>
2021-06-04 22:00:44 -0500

    Init state page

---
+++ components/Layout/LayoutAdmin.js
@@ -0,0 +1,12 @@
+const LayoutAdmin = ({ children }) => {
+  return (
+    <>
+      <main>
+        <h1>Layout Admin</h1>
+        {children}
+      </main>
+    </>
+  )
+}
+
+export default LayoutAdmin
---
+++ pages/admin/index.js
@@ -0,0 +1,13 @@
+import LayoutAdmin from '../../components/Layout/LayoutAdmin'
+
+const Admin = () => {
+  return (
+    <>
+      <LayoutAdmin>
+        <h1>Admin</h1>
+      </LayoutAdmin>
+    </>
+  )
+}
+
+export default Admin
