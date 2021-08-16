57fd55fe515379255aca88750a66f2acbae13630
andresdev <andresmestra11a@gmail.com>
2021-08-15 23:33:19 -0500

    responsive projects

--- components/me/Projects.js
+++ components/me/Projects.js
@@ -2,34 +2,19 @@ import { CardProject } from '../ui/cards/CardProject'
 import { HeaderLarge } from '../ui/headers/HeaderLarge'
 import { projects } from '../../utils/projects'
 
+import sty from '../../styles/components/me/projects.module.scss'
+
 export const Projects = () => {
   return (
     <section className="container_grid" id="projects">
-      <div className="projects_header">
+      <div className={sty.projects_header}>
         <HeaderLarge title="Proyectos" />
       </div>
-      <div className="projects_content">
+      <div className={sty.projects_content}>
         {projects.map((project) => (
           <CardProject key={project.name} {...project} />
         ))}
       </div>
-      <style jsx>{`
-        #projects {
-          padding: 5rem 0;
-        }
-
-        .projects_header {
-          grid-column: center-start / center-end;
-          text-align: center;
-        }
-
-        .projects_content {
-          grid-column: center-start / center-end;
-          display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
-          grid-gap: 7rem;
-        }
-      `}</style>
     </section>
   )
 }
